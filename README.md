4a3543aa7f7f0f0abff10f9c470dc029f4e3257b
Nicholas <nicholashendrata110900@gmail.com>
2021-04-15 00:05:53 +0700

    wrote company setup queries

--- README.md
+++ README.md
@@ -29,10 +29,11 @@ Uses Ant Design for UI components.
 #### Mid Priority Issues
 
 - Prevent new Marking Table and Item Table entries from being added with empty values.
-- In-Form tables such as marking tables and item tables should have a max height and scroll.
 
 #### Low Priority Issues
 
+- Marking and Item table input fields overlap with their labels.
+- In-Form tables such as marking tables and item tables should have a max height and scroll.
 - Calculator should automatically focus on Input when opened.
 - Inputting long text in the To-Do-List Input gets covered by the plus icon.
 - Replace Encryption.json with a local environment variable.
--- src/pages/CompanySetup.tsx
+++ src/pages/CompanySetup.tsx
@@ -1,20 +1,42 @@
-import React, { Component } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Typography } from 'antd';
 
 import { simpleQuery, query } from '../utils/query';
-import {} from '../Queries.json';
+
+import { companySetup } from '../Queries.json';
+const { formQuery, updateQuery, initializeQuery } = companySetup;
 
 const { Title, Text } = Typography;
 
-class CompanySetup extends Component {
-  render() {
-    return (
-      <CompanySetupStyles>
-        {/* here */}
-      </CompanySetupStyles>
-    );
+interface ICompanySettings {
+  companyname: string
+  address: string
+  city: string
+  phone: string
+  zipcode: number
+  fax: string
+  email: string
+}
+
+const CompanySetup: FC = () => {
+  const [companySettings, setCompanySettings] = useState<ICompanySettings>();
+  useEffect(() => { refreshData() }, []);
+
+  async function refreshData() {
+    const results = await simpleQuery(formQuery) as Array<ICompanySettings>;
+    if (results && results.length !== 0) {
+      const data = results[0];
+      setCompanySettings(data);
+    }
   }
+
+  console.log(companySettings);
+  return (
+    <CompanySetupStyles>
+      {/* here */}
+    </CompanySetupStyles>
+  );
 }
 
 export default CompanySetup;
